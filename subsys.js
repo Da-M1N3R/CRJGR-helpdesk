45424c4405e26ec62fa61931baef30bd80218421
Edgar <uranium322win@gmail.com>
2024-07-07 23:41:38 +0800

    debugged, use more console.log to debug

--- subsys.js
+++ subsys.js
@@ -1,20 +1,34 @@
-const gallery = document.querySelector(".comms");
+const comms = document.querySelector(".comms");
 
 function commsSubSys(subsys) {
+  let itemHTML = '';
+  let tempHTML = '';
+
+  for (let i = 0; i < subsys.name.length; i++) {
+    itemHTML += `
+      <span class="subsys-name">${subsys.name}</span><br>
+    `;
+    for (let j = 0; j < subsys.point.length; j++) {
+      
+      itemHTML += `
+        <span class="subsys-point">${subsys.point[j]}</span><span>${subsys.details[j]}</span><br>
+      `; 
+    }
+  }
   return `
     <div class="subsys">
-      <span class="subsys-name">${subsys.name}</span><br>
-      <span class="subsys-point">${subsys.point}</span><span>${subsys.details}</span><br>
-      <span class="subsys-point">${subsys.point2}</span><span>${subsys.details2}</span><br>
-      <span class="subsys-point">${subsys.point3}</span><span>${subsys.details3}</span>
+      ${itemHTML}
     </div>
   `;
 }
 
-fetch('details.json').then(response => response.json()).then(data => {
-  const characters = data;
-  characters.forEach(character => {
-    const cardHTML = commsSubSys(character);
-    gallery.innerHTML += cardHTML;
+fetch('details.json')
+.then(response => response.json())
+.then(data => {
+  const subsystems = data;
+  subsystems.forEach(subsysx => {
+    const subsysHTML = commsSubSys(subsysx);
+    comms.innerHTML += subsysHTML;
   })
-}).catch(error => console.error(error));
+})
+.catch(error => console.error(error));
